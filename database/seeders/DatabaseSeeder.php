53cb7402e8ca1bf1b797093ba6cf2d54d90d6cd5
Thierry Bifou <thierrybifou07@gmail.com>
2026-03-03 22:24:50 +0100

    call the ResidenceStatusSeeder in the DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\UserStatus;
+use App\Models\ResidenceStatus;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Foundation\Auth\User as AuthUser;
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RolePermissionSeeder::class,
             UserSeeder::class,
+            ResidenceStatusSeeder::class
         ]);
         // Select status active by default
         $activeId = UserStatus::getIdByCode(UserStatus::ACTIVE);
