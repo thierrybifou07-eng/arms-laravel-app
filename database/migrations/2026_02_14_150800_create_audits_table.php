3440b0a97fd3bc5b82900f420717a0d9b4fe411a
Thierry Bifou <thierrybifou07@gmail.com>
2026-03-03 20:05:55 +0100

    Reorder the migration files

--- database/migrations/2026_02_14_150800_create_audits_table.php
+++ database/migrations/2026_02_14_150800_create_audits_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('audit_logs', function (Blueprint $table) {
+        Schema::create('audits', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')
             ->nullable()
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('audit_logs');
+        Schema::dropIfExists('audits');
     }
 };
