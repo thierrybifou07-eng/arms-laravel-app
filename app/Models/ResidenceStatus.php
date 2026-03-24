24ac2a084656b1c91bedf21f8c66945531545b3d
Gedeon BIFOU <thierrybifou07@gmail.com>
2026-03-24 16:41:01 -0700

    Update ResidenceStatus.php

--- app/Models/ResidenceStatus.php
+++ app/Models/ResidenceStatus.php
@@ -8,8 +8,6 @@ class ResidenceStatus extends Model
 {
     protected $fillable = ['code', 'label'];
 
-    public const PENDING = 'pending';
-
     public const ACTIVE = 'active';
 
     public const CLOSED = 'closed';
