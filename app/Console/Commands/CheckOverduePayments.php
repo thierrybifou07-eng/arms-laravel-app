ac7faba9d29e57f030dba0d430f03773e07cf3d1
Gedeon Bifou <thierrybifou07@gmail.com>
2026-03-25 13:44:34 +0100

    Create CheckOverduePayments.php

---
+++ app/Console/Commands/CheckOverduePayments.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckOverduePayments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:check-overdue-payments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
