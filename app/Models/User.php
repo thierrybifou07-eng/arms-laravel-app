ddb266a952e1ffd2ce11391b9158e2ef7bb22031
Gedeon BIFOU <thierrybifou07@gmail.com>
2026-04-23 11:42:11 +0100

    remove email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,11 +10,10 @@ use OwenIt\Auditing\Contracts\Auditable as AuditableContract;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class User extends Authenticatable implements AuditableContract, HasMedia, MustVerifyEmail
+class User extends Authenticatable implements AuditableContract, HasMedia
 {
     use Auditable;
     use InteractsWithMedia;
-    use MustVerifyEmailTrait;
 
     public function avatar()
     {
